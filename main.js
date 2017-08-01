bd28242fa6b19ae7c1de54d9283a10c7639077ae
Jordan Benner <jordanbenner@outlook.com>
2017-08-01 14:54:03 -0500

    Changed the zoom
    6 to 8

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 29.7604, lng: 95.3698},
-      zoom: 6
+      zoom: 8
     });
     infoWindow = new google.maps.InfoWindow;
 
