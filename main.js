adea259b2bb18c7cf723511d2bd414f7c80e04af
Jordan Benner <jordanbenner@outlook.com>
2017-08-07 11:52:56 -0500

    Run function test
    Last function test

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
       zoom: 8
     });
     infoWindow = new google.maps.InfoWindow;
-    
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
@@ -27,10 +27,10 @@
     }
   }
 //
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  }
+  // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  //   infoWindow.setPosition(pos);
+  //   infoWindow.setContent(browserHasGeolocation ?
+  //                         'Error: The Geolocation service failed.' :
+  //                         'Error: Your browser doesn\'t support geolocation.');
+  //   infoWindow.open(map);
+  // }
