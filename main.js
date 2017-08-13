626aac2c2387caaa74b1927b0aa169603c66d868
Jordan Benner <jordanbenner@outlook.com>
2017-08-13 18:43:05 -0500

    Rubiks cube solver
    Yui.add

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
 
-  var map, infoWindow;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 29.7604, lng: 95.3698},
-      zoom: 8
-    });
-    infoWindow = new google.maps.InfoWindow;
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
+YUI.add('rubik-queue', function (Y) {
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  }
-//
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  }
+    Queue = function (config) {
+        config = config || {};
+        this.size = 0;
+        this.current = -1;
+        this._queue = [];
+    };
 
-  var str = "The best things in life are free";
-  var patt = new RegExp("e");
-  var res = patt.test(str);
+    Queue.prototype = {
+
+        add: function (m) {
+            if(++this.current == this.size){
+                this._queue.push(m);
+                this.size++;
+            }else{
+                this._queue = this._queue.slice(0,this.current);
+                this.size = this.current + 1;
+                this._queue.push(m);
+            }
+        },
+        undo: function () {
+            var m;
+            if(this.current >= 0){
+                m = this._queue[this.current];
+                m.rotate = m.rotate === 'left'? 'right' : 'left';
+                this.current--;
+                return m;
+            }
+        },
+        redo: function () {
+            if (this.current + 1 < this.size){
+                var m = this._queue[++this.current];
+                m.rotate = m.rotate === 'left'? 'right' : 'left';
+                return m;
+            }
+        }
+
+    };
+    Y.Queue = Queue;
+});
