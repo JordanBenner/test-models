a080aabc45e8677f4794443cfdaadc3bfdb77199
Jordan Benner <jordanbenner@outlook.com>
2017-08-15 16:35:42 -0500

    Test run prime
    Run prime number

--- main.js
+++ main.js
@@ -1,43 +1,30 @@
 
 
-YUI.add('rubik-queue', function (Y) {
-
-    Queue = function (config) {
-        config = config || {};
-        this.size = 0;
-        this.current = -1;
-        this._queue = [];
-    };
-
-    Queue.prototype = {
-
-        add: function (m) {
-            if(++this.current == this.size){
-                this._queue.push(m);
-                this.size++;
-            }else{
-                this._queue = this._queue.slice(0,this.current);
-                this.size = this.current + 1;
-                this._queue.push(m);
-            }
-        },
-        undo: function () {
-            var m;
-            if(this.current >= 0){
-                m = this._queue[this.current];
-                m.rotate = m.rotate === 'left'? 'right' : 'left';
-                this.current--;
-                return m;
-            }
-        },
-        redo: function () {
-            if (this.current + 1 < this.size){
-                var m = this._queue[++this.current];
-                m.rotate = m.rotate === 'left'? 'right' : 'left';
-                return m;
-            }
-        }
-
-    };
-    Y.Queue = Queue;
-});
+if(isPrime(ctr)==true)
+ {
+  primeNumbs[primeNumbs.length] = ctr;
+  }
+  ctr = ctr+1;
+}
+ if (primeNumbs.length == 0)
+{
+document.getElementById('output_content').innerHTML = "There were no prime no within the range.";
+ }
+ else
+ {
+ outputPrimeNums(primeNumbs);
+  }
+}
+function isPrime(num)
+{
+var flag = true;
+for(var i=2; i<=Math.ceil(num/2); i++)
+ {
+if((num%i)==0)
+{
+flag = false;
+ break;
+}
+}
+return flag;
+ }
