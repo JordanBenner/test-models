108e42ca7a262d84e44e88142ec0a50b33d2ed67
Jordan Benner <jordanbenner@outlook.com>
2017-08-21 10:24:02 -0500

    Removed prime function

--- main.js
+++ main.js
@@ -1,34 +1,4 @@
 
-
-if(isPrime(ctr)==true)
- {
-  primeNumbs[primeNumbs.length] = ctr;
-  }
-  ctr = ctr+1;
-}
- if (primeNumbs.length == 0)
-{
-document.getElementById('output_content').innerHTML = "There were no prime no within the range.";
- }
- else
- {
- outputPrimeNums(primeNumbs);
-  }
-}
-function isPrime(num)
-{
-var flag = true;
-for(var i=2; i<=Math.ceil(num/2); i++)
- {
-if((num%i)==0)
-{
-flag = false;
- break;
-}
-}
-return flag;
- }
-
  function outputPrimeNums(primes)
  {
   var html = "<h2>Prime Numbers</h2>";
