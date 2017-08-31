6bcfd6e908cd0c55bb09bef81482b0b7dd44ac92
Jordan Benner <jordanbenner@outlook.com>
2017-08-31 17:03:51 -0500

    Selector function

--- main.js
+++ main.js
@@ -1,30 +1,19 @@
 
-var img1 = document.querySelector('.img-1');
-
-function loaded() {
-  // woo yey image loaded
+obj = { "table":"customers", "limit":20 };
+dbParam = JSON.stringify(obj);
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myObj = JSON.parse(this.responseText);
+        txt += "<table border='1'>"
+        for (x in myObj) {
+            txt += "<tr><td>" + myObj[x].name + "</td></tr>";
+        }
+        txt += "</table>"
+        document.getElementById("demo").innerHTML = txt;
+    }
 }
-
-if (img1.complete) {
-  loaded();
-}
-else {
-  img1.addEventListener('load', loaded);
-}
-
-img1.addEventListener('error', function() {
-  // argh everything's broken
-});
-
-img1.callThisIfLoadedOrWhenLoaded(function() {
-  // loaded
-}).orIfFailedCallThis(function() {
-  // failed
-});
-
-// and…
-whenAllTheseHaveLoaded([img1, img2]).callThis(function() {
-  // all loaded
-}).orIfSomeFailedCallThis(function() {
-  // one or more failed
-});
+xmlhttp.open("POST", "json_demo_db_post.php", true);
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam);
+Try it Yourself »
