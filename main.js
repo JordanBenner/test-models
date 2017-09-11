83955487e5d874d5c7e44f76ffcd2b7b87f12c9a
Jordan Benner <jordanbenner@outlook.com>
2017-09-10 19:36:06 -0500

    Load dox

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 
-var words = str
-function showHint(str) {
-    if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "gethint.php?q=" + str, true);
-        xmlhttp.send();
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
     }
+  };
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
 }
