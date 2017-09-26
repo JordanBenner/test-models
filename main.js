a7a2249bd202bf645f0abb6fb89a6b7601bc84a9
Jordan Benner <jordanbenner@outlook.com>
2017-09-26 15:54:14 -0500

    Request url

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-var length = 16;
-var lastName = "Johnson";
-var x = {firstName:"John", lastName:"Doe"};    
+loadDoc("url-1", myFunction1);
+
+loadDoc("url-2", myFunction2);
+
+function loadDoc(url, cFunction) {
+  var xhttp;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      cFunction(this);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function myFunction1(xhttp) {
+  // action goes here
+}
+function myFunction2(xhttp) {
+  // action goes here
+}
