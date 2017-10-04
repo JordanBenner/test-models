330abd03998614652f7252e364d0822e4a7c0e10
Jordan Benner <jordanbenner@outlook.com>
2017-10-04 16:39:35 -0500

    Asp

--- main.js
+++ main.js
@@ -61,3 +61,19 @@ function myFunction(xml) {
   }
   document.getElementById("demo").innerHTML = table;
 }
+
+function showHint(str) {
+    if (str.length == 0) {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", "gethint.asp?q=" + str, true);
+        xmlhttp.send();
+    }
+}
