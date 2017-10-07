314163ba8a71f8eff61682f80d4ff9965d254198
Jordan Benner <jordanbenner@outlook.com>
2017-10-06 22:01:15 -0500

    Node

--- main.js
+++ main.js
@@ -84,3 +84,10 @@ function showHint(str) {
         xmlhttp.send();
     }
 }
+
+var para = document.createElement("p");
+var node = document.createTextNode("This is new.");
+para.appendChild(node);
+
+var element = document.getElementById("div1");
+element.appendChild(para);
