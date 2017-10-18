3ee91fb13046a09a7b16818b214f3c3990731122
Jordan Benner <jordanbenner@outlook.com>
2017-10-18 10:52:21 -0500

    Rm show hing

--- main.js
+++ main.js
@@ -68,16 +68,3 @@ function myFunction(xml) {
   }
   document.getElementById("demo").innerHTML = table;
 }
-
-function showHint(str) {
-    if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        
