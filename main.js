509eb09db861cbed4c0e7a95537ccd5c94c794b4
Jordan Benner <jordanbenner@outlook.com>
2017-10-20 12:18:24 -0500

    Load doc rm

--- main.js
+++ main.js
@@ -43,14 +43,3 @@ function showCustomer(str) {
   xhttp.open("GET", "getcustomer.asp?q="+str, true);
   xhttp.send();
 }
-
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    myFunction(this);
-    }
-  };
-  xhttp.open("GET", "cd_catalog.xml", true);
-  xhttp.send();
-}
