108ff48f78c6a64eb741f2cd706ce71f24451396
Jordan Benner <jordanbenner@outlook.com>
2017-11-01 18:29:31 -0500

    Show hint

--- main.js
+++ main.js
@@ -9,18 +9,3 @@ if (window.XMLHttpRequest) {
     // code for old IE browsers
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
-function showHint(str) {
-    if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "gethint.php?q=" + str, true);
-        xmlhttp.send();
-    }
-}
