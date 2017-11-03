32a4c7437b98800f0bc7549f8bbc5fea7961df6a
Jordan Benner <jordanbenner@outlook.com>
2017-11-03 15:51:42 -0500

    Pull data

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.name;
-    }
-};
-xmlhttp.open("GET", "json_demo.txt", true);
-xmlhttp.send();
+function showCustomer(str) {
+  var xhttp;
+  if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
