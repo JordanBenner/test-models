a9428ea39b2c8bd07a74750f4b408680bcdfd290
Jordan Benner <jordanbenner@outlook.com>
2018-03-17 17:03:15 -0500

    beautifuy

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-
-obj = { "table":"customers", "limit":20 };
+obj = {
+  "table": "customers",
+  "limit": 20
+};
 dbParam = JSON.stringify(obj);
 xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myObj = JSON.parse(this.responseText);
-        txt += "<table border='1'>"
-        for (x in myObj) {
-            txt += "<tr><td>" + myObj[x].name + "</td></tr>";
-        }
-        txt += "</table>"
-        document.getElementById("demo").innerHTML = txt;
+  if (this.readyState == 4 && this.status == 200) {
+    myObj = JSON.parse(this.responseText);
+    txt += "<table border='1'>"
+    for (x in myObj) {
+      txt += "<tr><td>" + myObj[x].name + "</td></tr>";
     }
+    txt += "</table>"
+    document.getElementById("demo").innerHTML = txt;
+  }
 }
 xmlhttp.open("POST", "json_demo_db_post.php", true);
 xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 xmlhttp.send("x=" + dbParam);
 
-
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    myFunction(this);
+      myFunction(this);
     }
   };
   xhttp.open("GET", "cd_catalog.xml", true);
@@ -31,14 +32,10 @@ function loadDoc() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
-  var table="<tr><th>Artist</th><th>Title</th></tr>";
+  var table = "<tr><th>Artist</th><th>Title</th></tr>";
   var x = xmlDoc.getElementsByTagName("CD");
-  for (i = 0; i <x.length; i++) {
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-    "</td></tr>";
+  for (i = 0; i < x.length; i++) {
+    table += "<tr><td>" + x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue + "</td><td>" + x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue + "</td></tr>";
   }
   document.getElementById("log").innerHTML = table;
 }
@@ -52,9 +49,9 @@ function showCustomer(str) {
   xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("txtHint").innerHTML = this.responseText;
+      document.getElementById("txtHint").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "getcustomer.asp?q="+str, true);
+  xhttp.open("GET", "getcustomer.asp?q=" + str, true);
   xhttp.send();
 }
